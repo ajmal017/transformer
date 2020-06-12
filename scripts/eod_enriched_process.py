998f8fc1f831695aaec10dee2f7899720f4b39f3
Transformer <fivethousand.below@gmail.com>
2020-06-12 10:47:59 -0400

    fix realtime for date

--- scripts/eod_enriched_process.py
+++ scripts/eod_enriched_process.py
@@ -1,5 +1,5 @@
 import sys,os
-from datetime import datetime
+from datetime import datetime,date
 from transformer_lib import util_funcs
 from transformer_lib import trading_calendar
 from transformer_lib import util_email
@@ -10,11 +10,11 @@ def process_eod_enrich( date ):
     print( 'processing for ', date )
     t = util_funcs.get_close_snap_enriched( 'VXX', date )
     fn = 'VXX_' + date.strftime('%Y%m%d') + '.csv'
-    t.to_csv( FILEPATH + fn)
+    t.to_csv( FILEPATH + fn, index=False)
     
     t = util_funcs.get_close_snap_enriched( 'SPY', date )
     fn = 'SPY_' + date.strftime('%Y%m%d') + '.csv'
-    t.to_csv( FILEPATH + fn)
+    t.to_csv( FILEPATH + fn, index=False)
     return
 
 if __name__ == '__main__':
@@ -25,7 +25,8 @@ if __name__ == '__main__':
         day = datetime.strptime( datestr, '%Y%m%d' )
         process_eod_enrich( day )
     else :
-        prev_bday = trading_calendar.getTPlusNDate( datetime.today(), -1)
+        dt = datetime.combine(date.today(), datetime.min.time())
+        prev_bday = trading_calendar.getTPlusNDate( dt, -1)
         process_eod_enrich(prev_bday)
     
     util_email.transformer_send_email('love from transformer bot','i sweetly finished eod enrichment work, xxoo')
